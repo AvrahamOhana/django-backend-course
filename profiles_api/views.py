bada0da390c9273315942dc3c98f521016db5825
AvrahamOhana <amiko617@gmail.com>
2024-02-15 10:38:16 +0200

    Add POST PATCH POST DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
+
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API View
     """
-    
+    serializer_class = serializers.HelloSerializer
     def get(self, request, format=None):
         """Get a list of APIView features
         """
@@ -17,3 +21,25 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs',
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})        
+
+    def post(self, request):
+        """Create a hello massage with our api"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            massage = f'Hello {name}'
+            return Response({'massage': massage})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+    def put(self, request, pk=None):
+        """HAndle updating an object"""
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """delete an object"""
+        return Response({'method': 'DELETE'})
